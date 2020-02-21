3cb8277fb30519e4efb004acd9692aa28164f463
beater <beater>
2020-02-21 13:43:41 +0800

    Initial commit

---
+++ src/main/kotlin/com/presisco/sql2graph/Blog.kt
@@ -0,0 +1,149 @@
+package com.presisco.sql2graph
+
+data class Blog(
+    val mid: String,
+    val childs: MutableSet<Blog> = hashSetOf(),
+    var stage: Int = 0,
+    var maxDepth: Int = 0,
+    var valid: Boolean = false
+) {
+    override fun hashCode(): Int {
+        return mid.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return when(other){
+            is Blog -> this.mid == other.mid
+            is String -> this.mid == other
+            else -> false
+        }
+    }
+
+    fun addChild(blog: Blog) = childs.add(blog)
+
+    companion object {
+
+        fun buildWoods(iterator: Iterator<Map<String, *>>): Pair<Map<String, Blog>, Set<Blog>> {
+            val blogs = hashMapOf<String, Blog>()
+            val roots = hashSetOf<Blog>()
+            while (iterator.hasNext()) {
+                val row = iterator.next()
+
+                val mid = row.getString("mid")
+                if (!blogs.containsKey(mid)) {
+                    blogs[mid] = Blog(mid)
+                    blogs[mid]!!.valid = true
+                }
+
+                if (row["repost_id"] != null) {
+                    val repostId = row.getString("repost_id")
+                    if (!blogs.containsKey(repostId)) {
+                        blogs[repostId] = Blog(repostId)
+                        roots.add(blogs[repostId]!!)
+                    }
+                    roots.remove(blogs[mid]!!)
+                    blogs[repostId]!!.addChild(blogs[mid]!!)
+                } else {
+                    roots.add(blogs[mid]!!)
+                }
+            }
+            return blogs to roots
+        }
+
+        fun calcDepths(roots: Set<Blog>): Map<Blog, Int> {
+            val depths = hashMapOf<Blog, Int>()
+            for (root in roots) {
+                depths[root] = maxDepth(root)
+            }
+            return depths
+        }
+
+        fun diffusionWidth(blog: Blog, depth: Int = 0, stages: MutableList<Int> = arrayListOf(0)): List<Int> {
+            if (depth == stages.size) {
+                stages.add(0)
+            }
+            stages[depth]++
+            blog.childs.forEach { diffusionWidth(it, depth + 1, stages) }
+            return stages
+        }
+
+        fun maxDepth(blog: Blog, level: Int = 0): Int {
+            blog.stage = level
+            if (blog.childs.isEmpty()) {
+                blog.maxDepth = level
+                return level
+            }
+            blog.childs.forEach { maxDepth(it, level + 1) }
+            val deepest = blog.childs.maxBy { it.maxDepth }
+            blog.maxDepth = deepest!!.maxDepth
+            return blog.maxDepth
+        }
+
+        fun longestPath(blog: Blog): MutableList<String> {
+            if (blog.childs.isEmpty()) {
+                return mutableListOf(blog.mid)
+            }
+
+            val deepestChild = blog.childs.maxBy { it.maxDepth }
+            val childPath = longestPath(deepestChild!!)
+            childPath.add(0, blog.mid)
+            return childPath
+        }
+
+        fun blogsAtDistance(
+            root: Blog,
+            distance: Int,
+            current: Int = 0,
+            blogs: MutableSet<String> = hashSetOf(),
+            steps: String = ""
+        ): Set<String> {
+            val currentSteps = if (steps == "") {
+                root.mid
+            } else {
+                steps + root.mid
+            }
+
+            if (distance == current) {
+                blogs.add(currentSteps)
+                return emptySet()
+            }
+
+            if (root.childs.isEmpty())
+                return emptySet()
+
+            root.childs.forEach {
+                blogsAtDistance(
+                    it,
+                    distance,
+                    current + 1,
+                    blogs,
+                    "$currentSteps, "
+                )
+            }
+
+            return if (current == 0)
+                blogs
+            else
+                emptySet()
+        }
+
+        fun averageNeighbors(
+            root: Blog,
+            depth: Int = 0,
+            neighbors: MutableSet<Int> = mutableSetOf()
+        ): Int {
+            if (root.childs.isEmpty()) {
+                return 0
+            }
+
+            neighbors.add(root.childs.size)
+            root.childs.forEach { averageNeighbors(it, depth + 1, neighbors) }
+
+            return if (depth == 0) {
+                neighbors.average().toInt()
+            } else {
+                0
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/presisco/sql2graph/BlogTreeAnalyze.kt
@@ -0,0 +1,80 @@
+package com.presisco.sql2graph
+
+import com.presisco.lazyjdbc.client.MapJdbcClient
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+
+object BlogTreeAnalyze {
+
+    val db = MapJdbcClient(
+        HikariDataSource(
+            HikariConfig(
+                mapOf(
+                    "dataSourceClassName" to "com.mysql.cj.jdbc.MysqlDataSource",
+                    "dataSource.url" to "jdbc:mysql://localhost:3306/weibo?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC",
+                    "dataSource.user" to "root",
+                    "dataSource.password" to "experimental",
+                    "maximumPoolSize" to "1"
+                ).toProperties()
+            )
+        )
+    )
+
+    fun setRootPointer(rootMid: String, blog: Blog, updateList: MutableList<Map<String, *>>) {
+        updateList.addAll(blog.childs.map { mapOf("mid" to it.mid, "root_id" to rootMid, "stage" to it.stage) })
+        blog.childs.forEach { setRootPointer(rootMid, it, updateList) }
+    }
+
+    @JvmStatic
+    fun main(vararg args: String) {
+        val iterator = db.selectIterator("select mid, repost_id from blog")
+
+        val woods = Blog.buildWoods(iterator)
+        val blogs = woods.first
+        val roots = woods.second
+        val rootDepths = hashMapOf<Blog, Int>()
+
+        println("unique blogs: ${blogs.keys.size}")
+        println("trees: ${roots.size}")
+
+        val rootUpdateList = arrayListOf<Map<String, *>>()
+        val blogUpdateList = arrayListOf<Map<String, *>>()
+        for (blog in roots) {
+            rootDepths[blog] = Blog.maxDepth(blog)
+            blogUpdateList.add(
+                mapOf(
+                    "mid" to blog.mid, "root_id" to blog.mid, "stage" to blog.stage
+                )
+            )
+            setRootPointer(blog.mid, blog, blogUpdateList)
+            rootUpdateList.add(
+                mapOf(
+                    "mid" to blog.mid,
+                    "depth" to rootDepths[blog]
+                )
+            )
+        }
+
+        val depthMap = rootDepths.entries.groupBy { it.value }.toSortedMap()
+
+        depthMap.forEach { (depth, mids) ->
+            println("depth: $depth has ${mids.size} trees")
+        }
+        db.deleteFrom("root").where("mid", "not in", roots.map { it.mid }.toList()).execute()
+        db.executeBatch(
+            { "update root set depth = ? where mid = ?" },
+            rootUpdateList,
+            db.buildTypeMapSubset("root", rootUpdateList),
+            listOf("depth", "mid")
+        )
+        db.executeBatch(
+            { "update blog set root_id = ?, stage = ? where mid = ?" },
+            blogUpdateList,
+            db.buildTypeMapSubset("blog", blogUpdateList),
+            listOf("root_id", "stage", "mid")
+        )
+
+        println("analyze finished!")
+    }
+
+}
